78d47bbb4612aa06def1f780d9311105194333ce
spacecadet85 <keith.reynolds.85@gmail.com>
2021-12-04 09:24:16 -1000

    adding ads1115 docs

--- GPS/start_GPS.sh
+++ GPS/start_GPS.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-sudo gpsd /dev/ttyAMA0 -F /var/run/gpsd.sock -n
+suod stty -F /dev/ttyS0 38400
+
+sudo systemctl disable gpsd.socket
+
+sudo gpsd /dev/ttyS0 -F /var/run/gpsd.sock
 
 cgps -s
 
