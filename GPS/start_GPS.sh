07fa4993cbc7aca882be6f0a44fb2eb4ee0403f8
spacecadet85 <83600449+spacecadet85@users.noreply.github.com>
2021-12-19 14:57:32 -1000

    udated wit hciuart

--- GPS/start_GPS.sh
+++ GPS/start_GPS.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-sudo stty -F /dev/ttyS0 9600 
+sudo stty -F /dev/ttyS0 38400
 
-sudo systemctl disable gpsd.socket
+#sudo systemctl disable gpsd.socket
+
+sudo systemctl disable hciuart
 
 sudo gpsd /dev/ttyS0 -F /var/run/gpsd.sock
 
