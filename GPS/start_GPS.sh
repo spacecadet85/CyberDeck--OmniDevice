c99368cbbaf1feb2d9af87677fbeac0e687b648e
spacecadet85 <keith.reynolds.85@gmail.com>
2021-12-19 14:52:08 -1000

    added stuff

--- GPS/start_GPS.sh
+++ GPS/start_GPS.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-sudo stty -F /dev/ttyS0 9600 
+sudo stty -F /dev/ttyAMA0 38400 
 
-sudo systemctl disable gpsd.socket
+#sudo systemctl disable gpsd.socket
+sudo systemctl disable hciuart
 
-sudo gpsd /dev/ttyS0 -F /var/run/gpsd.sock
+sudo gpsd /dev/ttyAMA0 -F /var/run/gpsd.sock
 
 cgps -s
 
