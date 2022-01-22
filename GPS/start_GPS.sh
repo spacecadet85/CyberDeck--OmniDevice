b153ee2f7cc688e6697f49e4c75da244ea23af8e
spacecadet85 <keith.reynolds.85@gmail.com>
2022-01-22 15:59:26 -0800

    fixing gps script

--- GPS/start_GPS.sh
+++ GPS/start_GPS.sh
@@ -1,18 +1,10 @@
 #!/bin/bash
 
-<<<<<<< HEAD
-sudo stty -F /dev/ttyAMA0 38400
-
-#sudo systemctl disable gpsd.socket
-=======
-sudo stty -F /dev/ttyS0 38400
-
-#sudo systemctl disable gpsd.socket
-
->>>>>>> 07fa4993cbc7aca882be6f0a44fb2eb4ee0403f8
+sudo stty -F /dev/ttyAMA1 38400
+sudo systemctl disable gpsd.socket
 sudo systemctl disable hciuart
 
-sudo gpsd /dev/ttyAMA0 -F /var/run/gpsd.sock
+sudo gpsd /dev/ttyAMA1 -F /var/run/gpsd.sock
 
 cgps -s
 
