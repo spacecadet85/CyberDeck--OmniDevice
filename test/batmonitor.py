c4ab77d78481e3a41d9e00d88726bdffc833d6cc
spacecadet85 <k.reynolds3@students.clark.edu>
2022-04-06 18:45:58 -0700

    adding moving average

--- test/batmonitor.py
+++ test/batmonitor.py
@@ -19,9 +19,19 @@ ads = ADS.ADS1115(i2c)
 # Create differential input between channel 0 and 1
 chan = AnalogIn(ads, ADS.P0, ADS.P3)
 voltageDivider = 0.179
+movingAvgBuff = [0,0,0,0,0,0,0,0,0,0]
+coutn = 0
+avg = 0.000
 
-print("{:>5}\t{:>5}\t{:>5}".format("raw", "v", "bat voltage"))
+print("{:>5}\t{:>5}\t{:>5}\t{:>5}".format("raw", "v", "bat voltage, moving avg"))
 
 while True:
-    print("{:>5}\t{:>5.3f}\t{:5.3f}".format(chan.value, chan.voltage, chan.voltage / voltageDivider))
+    batvoltage = chan.voltage / voltageDivider
+    movingAvgBuff[count] = batvoltage
+    count = count + 1
+    if count >= 9:
+        avg = sum(movingAvgBuff)/len(movingAvgBuff)
+        count = 0
+    print("{:>5}\t{:>5.3f}\t{:5.3f}\t{:5.3f}".format(chan.value, chan.voltage, batvoltage, avg))
+
     time.sleep(1)
